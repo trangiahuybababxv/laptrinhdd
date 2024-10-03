135e3f0b5069a0c0da964e7a2bb7715dc4031399
trangiahuybababxv <huytg.22ceb@vku.udn.vn>
2024-10-03 19:08:55 +0700

    lab3

---
+++ lab3/Decoration.kt
@@ -0,0 +1,24 @@
+package example3.myapp.decor
+
+data class Decoration(val rocks: String) {
+}
+
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+
+    val decoration2 = Decoration("slate")
+    println(decoration2)
+
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+
+    println (decoration1.equals(decoration2))
+    println (decoration3.equals(decoration2))
+
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
---
+++ lab3/Main.kt
@@ -0,0 +1,15 @@
+package example3.myapp.decor
+
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+
+    makeDecorations()
+
+}
